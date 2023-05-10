03eb54b1f144800974e5c45faecee59b42818e38
Kanata Miyahana <afiri.bupa@gmail.com>
2023-05-10 23:51:36 +0900

    fix

--- application.go
+++ application.go
@@ -1,14 +1,42 @@
 package ungo
 
 import (
-	"github.com/k3forx/ungo/pkg/action"
+	"errors"
+	"fmt"
+	"strings"
+
 	"github.com/urfave/cli/v2"
 )
 
+const (
+	defaultEmoji = "💩"
+)
+
 func NewApp() *cli.App {
+	var (
+		emojiCount int
+	)
 	return &cli.App{
-		Name:   "boom",
-		Usage:  "make an explosive entrance",
-		Action: action.NewAction(),
+		Name:  "ungo",
+		Usage: "ungo adds emoji of shit at suffix",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:        "count",
+				Aliases:     []string{"cnt", "c"},
+				Value:       1,
+				Usage:       "specify the number of emoji",
+				Destination: &emojiCount,
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			if cCtx.Args().Len() == 0 {
+				return errors.New("need to specify some text")
+			}
+
+			text := cCtx.Args().First()
+			emoji := strings.Repeat(defaultEmoji, emojiCount)
+			fmt.Printf("%s%v\n", text, emoji)
+			return nil
+		},
 	}
 }
--- pkg/action/action.go
+++ pkg/action/action.go
@@ -1,14 +1,18 @@
 package action
 
 import (
-	"fmt"
-
 	"github.com/urfave/cli/v2"
 )
 
 func NewAction() func(cCtx *cli.Context) error {
 	return func(cCtx *cli.Context) error {
-		fmt.Printf("Hello %q", cCtx.Args().Get(0))
+		// if cCtx.Args().Len() == 0 {
+		// 	return nil
+		// }
+
+		// text := cCtx.Args().First()
+		// // emoji := strings.Repeat(defaultEmoji, ungo.GetNum)
+		// fmt.Printf("%s%v\n", text, emoji)
 		return nil
 	}
 }
